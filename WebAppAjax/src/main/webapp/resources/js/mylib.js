9fcd857400cefc4f846f836373029a94efb15b28
Paulo Sérgio <paulossjunior@gmail.com>
2013-08-26 14:19:13 -0300

    Modificação das libs e result.xhtml

--- WebAppAjax/src/main/webapp/resources/js/mylib.js
+++ WebAppAjax/src/main/webapp/resources/js/mylib.js
@@ -1,22 +1,8 @@
-function ajaxCall(){
-	$.ajax({
-		type: "get",
-		url: "http://localhost:8080/webappajax/isWorking",
-		cache: false,				
-		success: function(response){
-			$('#result').html("");
-			$('#result').html("Mensagem: "+response);
-		},
-		error: function(response, status, error){
-			$('#result').html("");
-			$('#result').html("Error: "+error);
-		}
-	});
-}
-
-function ajaxGetLocal(){
+//Funcao responsavel por buscar os locais que um usuario esta habilitado
+function ajaxGetLocation(){
 	
 	var val = $("#login").val();
+	
 	$.ajax({
 		type: "post",
 		url: "http://localhost:8080/webappajax/locais",
@@ -26,8 +12,9 @@ function ajaxGetLocal(){
         contentType: "application/json",
 		success: function(response){
 			
+			//Apagando os option que foram preenchidos
 			$('#local').find('option').remove().end();
-			
+			//Preenchendo o select com os valores do serviço
 			$.each(response,function(idx, obj){ 
 		        $.each(obj, function(key, value){
 		        	$("#local").append("<option value='" + value + "'>" + value + "</option>");
