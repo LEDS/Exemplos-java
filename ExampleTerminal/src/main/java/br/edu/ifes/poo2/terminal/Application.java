3dd0996209f6a4a25d0ddfb38c33ebf411beb9d9
Paulo Sérgio <paulossjunior@gmail.com>
2013-08-26 09:07:35 -0300

    Example de um terminal usando a API

---
+++ ExampleTerminal/src/main/java/br/edu/ifes/poo2/terminal/Application.java
@@ -0,0 +1,12 @@
+package br.edu.ifes.poo2.terminal;
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Oi mundo!");
+	}
+
+}
